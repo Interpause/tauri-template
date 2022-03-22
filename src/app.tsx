64b058597523a60b91fec48424affdbd1e448f7a
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-03-22 16:04:07 +0800

    prettier everything

--- src/app.tsx
+++ src/app.tsx
@@ -7,10 +7,10 @@ export function App() {
       <p>Hello Vite + Preact!</p>
       <p>
         <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
+          class='link'
+          href='https://preactjs.com/'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn Preact
         </a>
